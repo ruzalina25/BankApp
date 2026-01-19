4dd69c043282d6c3d9e014e6f8fbf295ce7ca73e
Ruzalina <ruzalina13@icloud.com>
2026-01-20 02:11:20 +0500

    feat: created BankAccount base class

---
+++ BankApp/Models/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Models
+{
+    public class BankAccount
+    {
+        public int AccountNumber;
+        public string Owner;
+        public decimal Balance;
+        
+        public BankAccount(int accountNumber, string owner, decimal balance)
+        {
+            AccountNumber = accountNumber;
+            Owner = owner;
+            Balance= balance;
+
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Cчет:{AccountNumber},Владелец:{Owner},Баланс:{Balance}");
+        }
+
+    }
+}
